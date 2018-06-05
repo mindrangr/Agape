8b05f0064f601375a49e23f5ff0e65b9f44e9628
Marcus Estes <marcus.estes@gasoc.com>
2018-06-05 07:32:09 -0400

    June 5, 2018

---
+++ templates/friends1.php
@@ -0,0 +1,128 @@
+<?php
+  session_start();
+
+  include("Config_files/_shared_config.php");
+
+?>
+<!Doctype html />
+  <script src="<?php echo $actual_base ?>/scripts/jquery-3.1.1.min.js"></script>  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" href="styles/NetscapeStyles_responsive1.css" />
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="scripts/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo $actual_base ?>/scripts/jTools.js"></script>
+<script src="scripts/jScripts.js"></script>
+
+<script src="<?php echo $actual_base ?>/scripts/TweenMax.js"></script>
+<!-- Modal -->
+<div class="modal fade" id="homePageModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div id="innerModal"></div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+ 
+<body style="padding-top:70px;background-image: url('images/blog1Blur.jpg');background-repeat:no-repeat;background-color: #2f4774;">
+ 
+  <?php include 'header.php';?>
+
+    
+     
+    <div class="container newsContainer" >
+      <div class="overF">
+        <div class="row">
+          <div class="col-sm-3 tPaddMobileLogin" >
+            <?php include 'templates/logBox.php';?>
+             
+          </div>
+      
+          <div class="col-sm-3">
+           
+          </div>
+           <div class="col-sm-3">
+           
+          </div>
+           <div class="col-sm-3">
+             
+            </div>
+            
+        
+      </div>
+    </div>
+  </div>
+
+   
+
+  <div class="container-fluid sponsorBackground socialbanner" >
+    <div class="row" id="sponsorDiv">
+      <div class="col-sm-2">
+        <div style="font-size:22px;">Meet our</div>
+        <div style="color:#c96cbd;text-shadow: 1px 1px #000;font-size:20px;">Sponsors:</div>
+      </div>
+    </div>
+  </div>
+
+ 
+
+     <div class="container-fluid sponsorBackground htClass" style="margin-top:0px;background-color:#c96cbd;" >
+    <div class="row" id="sponsorDiv" >
+      <div class="col-sm-2">
+        <div style="font-size:22px;">Social Connections</div>
+        
+      </div>
+
+      <div class="col-sm-1">
+        <div >Instagram</div>
+        <div><img src="images/instagram.png" style="height:40px" /></div>
+
+  
+      </div>
+       <div class="col-sm-1">
+
+        <div >Twitter</div>
+        <div><img src="images/twitter-logo_19.png" style="height:40px" /></div>
+
+        
+      </div>
+       <div class="col-sm-1">
+        <div >FaceBook</div>
+        <div><img src="images/facebook.jpg" style="height:40px" /></div>
+        
+      </div>
+    </div>
+  </div>
+
+
+   
+
+
+  <?php include 'footer.html';?>
+ 
+</body>
+
+<script>
+
+
+
+  
+  getSponsors();
+  //getOnlineUsers(global.userListCnt);
+
+
+
+</script>
